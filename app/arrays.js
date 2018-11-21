4dbe59efd9a7c2b62c21a76834818b9f36e5be38
Mohammad Anouti <manouti1988@gmail.com>
2018-11-21 08:23:36 +0200

    feat: array functions

--- app/arrays.js
+++ app/arrays.js
@@ -1,60 +1,85 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
-define(function() {
+define(function () {
   return {
-    indexOf : function(arr, item) {
+    indexOf: function (arr, item) {
       return arr.indexOf(item);
     },
 
-    sum : function(arr) {
-       
+    sum: function (arr) {
+      let total = 0;
+      for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+      }
+      return total;
     },
 
-    remove : function(arr, item) {
-
+    remove: function (arr, item) {
+      return arr.filter(function (ele) {
+        return ele != item;
+      });
     },
 
-    removeWithoutCopy : function(arr, item) {
-
+    removeWithoutCopy: function (arr, item) {
+      for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === item) {
+          arr.splice(i, 1);
+        }
+      }
+      return arr;
     },
 
-    append : function(arr, item) {
-
+    append: function (arr, item) { 
+      arr.push(item);
+      return arr;
     },
 
-    truncate : function(arr) {
-
+    truncate: function (arr) {
+      arr.splice(arr.length-1);
+      return arr;
     },
 
-    prepend : function(arr, item) {
-
-    },
-
-    curtail : function(arr) {
-
-    },
-
-    concat : function(arr1, arr2) {
-
-    },
-
-    insert : function(arr, item, index) {
-
-    },
-
-    count : function(arr, item) {
-
-    },
-
-    duplicates : function(arr) {
+    prepend: function (arr, item) {
+      arr.unshift(item);
+      return arr;
 
     },
 
-    square : function(arr) {
+    curtail: function (arr) {
 
     },
 
-    findAllOccurrences : function(arr, target) {
+    concat: function (arr1, arr2) {
+      return arr1.concat(arr2);
+    },
+
+    insert: function (arr, item, index) {
+
+    },
+
+    count: function (arr, item) {
+      var res = 0; 
+          
+      for (let i = 0; i < arr.length; i++) 
+          if (item == arr[i]) 
+          res++; 
+            
+      return res; 
+    },
+
+    duplicates: function (arr) {
+
+    },
+
+    square: function (arr) {
+      return arr.map(x=> {
+        return Math.pow(x, 2);
+      });
+    },
+
+    findAllOccurrences: function (arr, target) {
 
     }
   };
