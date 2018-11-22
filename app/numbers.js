54ddff6d35d2f4c21fc3bf911333a0c2b7ad61e7
Mohammad Anouti <manouti1988@gmail.com>
2018-11-22 09:44:57 +0200

    feat:number exercise

--- app/numbers.js
+++ app/numbers.js
@@ -3,15 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+      return 1 & (num >> (bit - 1));
     },
 
     base10: function(str) {
-
+      return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
+      var arr = [];
 
+      for (var i = 7; i > -1; i--) {
+        arr.push( num & (1 << i) ? 1 : 0 );
+      }
+  
+      return arr.join('');
     },
 
     multiply: function(a, b) {
