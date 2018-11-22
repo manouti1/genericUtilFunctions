e10b33df71b52c468c4852893d1747e51c9e4215
Mohammad Anouti <manouti1988@gmail.com>
2018-11-22 10:17:00 +0200

    feat:regex exercise

--- app/recursion.js
+++ app/recursion.js
@@ -3,11 +3,38 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
-
+      var listOfFiles = [];
+      var dirs = [];
+  
+      function processDir(dir) {
+        var i;
+        var len;
+        var file;
+        var files = dir.files;
+  
+        dirs.push( dir.dir );
+  
+        for (i = 0, len = files.length; i < len; i++) {
+          file = files[i];
+          if (typeof file === 'string') {
+            if (!dirName || dirs.indexOf(dirName) > -1) {
+              listOfFiles.push(files[i]);
+            }
+          } else {
+            processDir(files[i]);
+          }
+        }
+  
+        dirs.pop();
+      }
+  
+      processDir(data);
+  
+  
+      return listOfFiles;
     },
 
     permute: function(arr) {
-
     }
   };
 });
