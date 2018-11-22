ef8a78bdd2bbd2c73a01c56a0ebb226ad00be796
Mohammad Anouti <manouti1988@gmail.com>
2018-11-22 09:00:19 +0200

    feat:flow control exercise

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,8 +1,10 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
-define(function() {
+define(function () {
   return {
-    fizzBuzz : function(num) {
+    fizzBuzz: function (num) {
       // write a function that receives a number as its argument;
       // if the number is divisible by 3, the function should return 'fizz';
       // if the number is divisible by 5, the function should return 'buzz';
@@ -11,6 +13,19 @@ define(function() {
       //
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+      if (typeof num !== 'number') {
+        return false;
+      }
+      if (num % 3 === 0 && num % 5 === 0) {
+        return 'fizzbuzz';
+      }
+      if (num % 3 === 0) {
+        return 'fizz';
+      }
+      if (num % 5 === 0) {
+        return 'buzz';
+      }
+      return num;
     }
   };
 });
