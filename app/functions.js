4b86d661053feb9286786ba4506f1508b6b736b0
Mohammad Anouti <manouti1988@gmail.com>
2018-11-22 09:34:50 +0200

    feat:function exercises

--- app/functions.js
+++ app/functions.js
@@ -3,15 +3,17 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+      return fn.apply(null, arr);
     },
 
     speak : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+      return function(arg) {
+        return str + ', ' + arg;
+      };
     },
 
     makeClosures : function(arr, fn) {
@@ -19,15 +21,24 @@ define(function() {
     },
 
     partial : function(fn, str1, str2) {
-
+      return function(str3) {
+        return fn(str1, str2, str3);
+      };
     },
 
     useArguments : function() {
+      var sum = 0;
 
+      for (var i = 0, len = arguments.length; i < len; i++) {
+        sum += arguments[i];
+      }
+
+      return sum;
     },
 
     callIt : function(fn) {
-
+      var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+      fn.apply(null, args);
     },
 
     partialUsingArguments : function(fn) {
